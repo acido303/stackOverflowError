a127fc3c14cfb405b1ede002be4f0c9042429fd1
José Antonio Sánchez <acido303@gmail.com>
2019-05-09 02:05:03 +0100

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jasanchez on 09/05/2019.
+ */
+public class Main {
+
+    static int stackOverflow(int i){
+        return stackOverflow(i);
+    }
+
+    public static void main(String[] args) {
+        stackOverflow(1);
+    }
+}
